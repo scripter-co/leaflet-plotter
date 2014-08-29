69676d16645cba21c8660643fb62cad4d87dbee7
Nathan <mail@scripter.co>
2014-08-29 14:08:16 +0100

    update src file to latest version

--- src/leaflet.plotter.js
+++ src/leaflet.plotter.js
@@ -98,6 +98,7 @@ L.Polyline.plotter = L.Polyline.extend({
         }
         this._halfwayPointMarkers = [];
         for(index in this._lineMarkers){
+            index = parseInt(index);
             if(typeof this._lineMarkers[index + 1] === 'undefined'){
                 return;
             }
